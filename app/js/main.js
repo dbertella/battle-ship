ce4cfd365f946e3178f4eb05053fdb955481cc6d
dbertella <daniele.bertella@gmail.com>
2015-06-24 01:24:20 +0200

    game working without sinking control of ships and with prompt

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,23 @@
 (function() {
-	// store the array of object of coordinates
-	function protoCoordinate(xVal, yVal, array) {
-		array.push({
-		x: xVal,
-		y: yVal,
-		ship: false
-		});
-	}
-	// creating the field that take the size as a parameter
-	var createField = function(sizeXY) {
-	var axis = [],
-		alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
-
-	for (var y = 0; y < sizeXY; y++) {
-		for (var x = 0; x < sizeXY; x++) {
-		protoCoordinate(x, alphabet[y], axis);
-		}
-	}
-	return axis;
-	}
-	var battleField = createField(10);
-	var removedSquare = [];
 	
-	var createShip = function(ship) {
-		var index = random();
+	var removedSquare = [],
+		battleField = __createField(10);
+	
+	var positionRandomShip = function(ship) {
+		var index = __randomElement();
 
-		if (checkIfSpaceIsFree(index, ship.length)) {
-			battleField[index].ship = true;
+		if (__checkIfSpaceIsFree(index, ship.length)) {
+			battleField[index].ship = ship.name;
 			removedSquare = removedSquare.concat([index, index + 1, index -1, index + 10, index -10]);
 			
 			// if random is even the ship is in vertical position
 			if (index % 2 === 0) {
 				for (var inc = 1; inc < ship.length; inc++) {
 					if (battleField[index].x <= 5) {
-						battleField[index + inc].ship = true;
+						battleField[index + inc].ship = ship.name;
 						removedSquare = removedSquare.concat([index + inc, index + inc + 1, index + inc -1, index + inc + 10, index + inc -10]);
 					} else {
-						battleField[index - inc].ship = true;
+						battleField[index - inc].ship = ship.name;
 						removedSquare = removedSquare.concat([index - inc, index - inc + 1, index - inc -1, index - inc + 10, index - inc -10]);
 					}
 				}
@@ -44,42 +25,45 @@
 				// else is horizzontal 
 				for (var j = 1; j < ship.length; j++) {
 					if (index <= 50) {
-						battleField[index + j * 10].ship = true;
+						battleField[index + j * 10].ship = ship.name;
 						removedSquare = removedSquare.concat([index + j * 10, index + j * 10 + 1, index + j * 10 -1, index + j * 10 + 10, index + j * 10 -10]);
 					} else {
-						battleField[index - j * 10].ship = true;
+						battleField[index - j * 10].ship = ship.name;
 						removedSquare = removedSquare.concat([index - j * 10, index - j * 10 + 1, index - j * 10 -1, index - j * 10 + 10, index - j * 10 -10]);
 					}
 				}
 			}
 		} else {
-			console.log(index);
-			createShip(ship);
+			positionRandomShip(ship);
 		}
 	}
-	var positionShips = function() {
-		var ships = [{
-			length: 5,
-			name: "Battleship"
-		},
-		{
-			length: 4,
-			name: "Destroyer-1"
-		},
-		{
-			length: 4,
-			name: "Destroyer-2"
-		}];
-		for (var i = 0; i < ships.length; i++) {
-			createShip(ships[i]);
-		}
-		return ships;
-	}
+	var initGame = function() {
+		var ships = [
+			{
+				length: 5,
+				name: "battleship",
+				hit: 0
+			},
+			{
+				length: 4,
+				name: "destroyer-1",
+				hit: 0
+			},
+			{
+				length: 4,
+				name: "destroyer-2",
+				hit: 0
+			}
+		];
 
-	positionShips();
-	console.log(removedSquare);
+		for (var i = 0; i < ships.length; i++) {
+			positionRandomShip(ships[i]);
+		}
+
+	}
+	
 	// Check if the ship can stay in the space
-	function checkIfSpaceIsFree(index, shipLength) {
+	function __checkIfSpaceIsFree(index, shipLength) {
 		if (removedSquare.indexOf(index) === -1) { 
 			if (index % 2 === 0) {
 				for (var inc = 1; inc < shipLength; inc++) {
@@ -114,15 +98,83 @@
 	}
 
 	// Get a random position
-	function random() {
+	function __randomElement() {
 		return Math.floor(Math.random() * 100);
 	}
+	// store the array of object of coordinates
+	function __protoCoordinate(xVal, yVal, array) {
+		array.push({
+		x: xVal,
+		y: yVal,
+		ship: false
+		});
+	}
+	// creating the field that take the size as a parameter
+	function __createField(sizeXY) {
+		var axis = [],
+			alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
 
-	// visual part to see and debug
+		for (var y = 0; y < sizeXY; y++) {
+			for (var x = 0; x < sizeXY; x++) {
+			__protoCoordinate(x, alphabet[y], axis);
+			}
+		}
+		return axis;
+	}
+
+
+	// game initialization
+	initGame();
+
 	var battleFieldArray = battleField.map(function(s, i) {
 		return s.y + s.x;
 	});
 
+	function playerShooting () {
+		var playerShoot = prompt("Insert coordinate to shoot"),
+			alphabet = "abcdefghij".toUpperCase();
+
+
+
+		if (playerShoot != null) {
+			playerShoot = playerShoot.toUpperCase()
+			if(alphabet.indexOf(playerShoot[0]) > -1 && playerShoot[1] >= 0 && playerShoot[1] < 10 ) {
+
+			    var index = battleFieldArray.indexOf(playerShoot),
+			    	shipName;
+
+			    if (battleField[index].ship) {
+			    	shipName = battleField[index].ship;
+			    	battleField[index].ship = 'hit';
+			    	console.log('Player hit: ' + playerShoot);
+
+			    	playerShooting();
+			    } else {
+			    	battleField[index].ship = 'missed';
+			    	console.log('Player missed: ' + playerShoot);
+			    	cpuShooting();
+			    }
+			} else {
+				console.log('k0');
+				playerShooting();
+			}
+		}
+	}
+	function cpuShooting() {
+		var index = __randomElement();
+		if (battleField[index].ship) {
+			battleField[index].ship = 'hit';
+			console.log('Cpu hit' );
+			cpuShooting();
+		} else {
+			battleField[index].ship = 'missed';
+			console.log('Cpu missed');
+			playerShooting();
+		}
+	}
+	// visual part to see and debug
+	
+playerShooting();
 	var htmlBattleField = '';
 	for (var i = 1; i <= battleFieldArray.length; i++) {
 		htmlBattleField += '<span class="ship-' + battleField[i - 1].ship + '">' + battleFieldArray[i - 1] + '</span>';
